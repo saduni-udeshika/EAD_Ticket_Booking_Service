16d6d87ad7134612f2d86167fa65fae67a2a642d
saduni-udeshika <it20491994@my.sliit.lk>
2023-10-07 10:47:12 +0530

    initial build

--- Models/Train.cs
+++ Models/Train.cs
@@ -4,8 +4,8 @@ namespace TicketBookingService.Models
     public class Train
     {
         public ObjectId Id { get; set; }
-        public string TrainName { get; set; }
-        public string TrainNumber { get; set; }
+        public string? TrainName { get; set; }
+        public string? TrainNumber { get; set; }
     }
 
 }
